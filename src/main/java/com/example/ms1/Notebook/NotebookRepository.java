b5a645fea4afe57705cbfc3d5b521c85b7b1a386
Dowon Nam <dwnam0114@gmail.com>
2024-04-22 20:27:14 +0900

    making the mainController and etc

--- src/main/java/com/example/ms1/Notebook/NotebookRepository.java
+++ src/main/java/com/example/ms1/Notebook/NotebookRepository.java
@@ -1,4 +1,6 @@
 package com.example.ms1.Notebook;
 
-public interface NotebookRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NotebookRepository extends JpaRepository<Notebook,Long> {
 }
--- src/main/java/com/example/ms1/note/note/NoteService.java
+++ src/main/java/com/example/ms1/note/note/NoteService.java
@@ -1,4 +1,22 @@
 package com.example.ms1.note.note;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.print.DocFlavor;
+import java.time.LocalDateTime;
+
+@Service
+@RequiredArgsConstructor
 public class NoteService {
+
+    private final NoteRepository noteRepository;
+    public Note saveDefault(){
+        Note note = new Note();
+        note.setTitle("새로운 노트북 제목");
+        note.setTitle("빈 내용");
+        note.setCreateDate(LocalDateTime.now());
+
+        return noteRepository.save(note);
+    }
 }
